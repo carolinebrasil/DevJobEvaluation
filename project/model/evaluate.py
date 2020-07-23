e38264ba8cf5da3ce00ef60f66c22402881f840d
Caroline Brasil <icarolinebrasil@gmail.com>
2020-07-23 20:50:29 -0300

    Ajuste mensagem

--- project/model/evaluate.py
+++ project/model/evaluate.py
@@ -9,15 +9,15 @@ class Eval(object):
 
         if int(info['htmlskill']) >= 7 <= 10 and int(info['cssskill']) >= 7 <= 10 and int(info['javascriptskill']) >= 7 <= 10:
             frontend = 1
-            flash('Você é dev Frontend')
+            flash('Obrigado por se candidatar! Assim que tivermos uma vaga disponível para programador Front-End entraremos em contato.')
 
         if int(info['pythonskill']) >= 7 <= 10 and int(info['djangoskill']) >= 7 <= 10:
             backend = 1
-            flash('Você é dev Backend')
+            flash('Obrigado por se candidatar! Assim que tivermos uma vaga disponível para programador Back-End entraremos em contato.')
 
         if int(info['iosskill']) >= 7 <= 10 and int(info['androidskill']) >= 7 <= 10:
             mobile = 1
-            flash('Você é dev Mobile')
+            flash('Obrigado por se candidatar! Assim que tivermos uma vaga disponível para programador Mobile entraremos em contato.')
 
         if frontend != 1 and backend != 1 and mobile != 1:
-            flash('Você é dev generico')
+            flash('Obrigado por se candidatar! Assim que tivermos uma vaga disponível para programador entraremos em contato.')
